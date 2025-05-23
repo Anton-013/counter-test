4a00268ece2a7cd8ca443f3bbe3192fdc0efa56e
Anton-013 <antonchuvashovoren@mail.ru>
2025-05-23 18:19:51 +0300

    added styles for error

--- src/components/CounterConfiguration/CounterConfiguration.tsx
+++ src/components/CounterConfiguration/CounterConfiguration.tsx
@@ -13,8 +13,14 @@ export const CounterConfiguration = ({ setValue }: CounterConfigurationProps) =>
 
     const onClickSetHandler = () => {
         setValue(maxValue, initialValue)
-        setMaxValue(0)
-        setInitialValue(0)
+    }
+
+    const checkCondition = () => {
+        return initialValue < maxValue && initialValue >= 0
+    }
+
+    const disabledButton = () => {
+        return initialValue < maxValue && initialValue >= 0
     }
 
     return (
@@ -22,15 +28,17 @@ export const CounterConfiguration = ({ setValue }: CounterConfigurationProps) =>
             <div className={'block-input'}>
                 <div>
                     <span>max value</span>
-                    <Input setTitle={setMaxValue} title={maxValue} />
+                    <Input classes={maxValue > 0 ? 'input' : 'input-error'} setTitle={setMaxValue} title={maxValue} />
+                    <div className={maxValue > 0 ? 'input-hint' : 'input-hint-error'}>{maxValue > 0 ? 'enter number' : 'except zero and negative numbers'}</div>
                 </div>
                 <div>
                     <span>start value</span>
-                    <Input setTitle={setInitialValue} title={initialValue} />
+                    <Input classes={checkCondition() ? 'input' : 'input-error'} setTitle={setInitialValue} title={initialValue} />
+                    <div className={checkCondition() ? 'input-hint' : 'input-hint-error'}>{checkCondition() ? 'enter number' : 'from 0 to max value'}</div>
                 </div>
             </div>
             <div className={'block-button'}>
-                <Button name={'set'} onClick={onClickSetHandler} disabled={false} />
+                <Button name={'set'} onClick={onClickSetHandler} disabled={!disabledButton()} />
             </div>
         </div>
     )
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,17 +2,20 @@ import type { ChangeEvent } from "react"
 
 
 type InputProps = {
+    classes: string
     setTitle: (title: number) => void
     title: number
 }
 
-export const Input = ({title, setTitle}: InputProps) => {
+export const Input = ({ classes, title, setTitle }: InputProps) => {
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setTitle(Number(e.currentTarget.value))
     }
 
     return (
-        <input type='number' value={String(title)} onChange={onChangeHandler}/>
+        <>
+            <input className={classes} type='number' value={String(title)} onChange={onChangeHandler} />
+        </>
     )
 }
